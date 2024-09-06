653ca8f870f44cc51bc2b5a4bd264915f1c9f8d5
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-06 14:16:50 +0200

    add first base test

---
+++ tests/test_ilastik_pixel_classification_segmentation.py
@@ -0,0 +1,53 @@
+import shutil
+from pathlib import Path
+
+import pytest
+from devtools import debug
+from fractal_tasks_core.channels import ChannelInputModel
+
+from ilastik_tasks.ilastik_pixel_classification_segmentation import (
+    ilastik_pixel_classification_segmentation,
+)
+
+
+@pytest.fixture(scope="function")
+def test_data_dir(tmp_path: Path) -> str:
+    """
+    Copy a test-data folder into a temporary folder.
+    """
+    source_dir = (Path(__file__).parent / "data/ngff_example/my_image").as_posix()
+    dest_dir = (tmp_path / "my_image").as_posix()
+    debug(source_dir, dest_dir)
+    shutil.copytree(source_dir, dest_dir)
+    return dest_dir
+
+
+@pytest.fixture(scope="function")
+def test_ilastik_model(tmp_path: Path) -> str:
+    """
+    Copy a test-data folder into a temporary folder.
+    """
+    source_dir = (
+        Path(__file__).parent / "data/tests/data/pixel_classifier_2D.ilp"
+    ).as_posix()
+    dest_dir = (tmp_path / "my_image").as_posix()
+    debug(source_dir, dest_dir)
+    shutil.copytree(source_dir, dest_dir)
+    return dest_dir
+
+
+def test_thresholding_label_task(test_data_dir):
+    """
+    Test the ilastik_pixel_classification_segmentation task.
+    """
+    ilastik_model = (
+        Path(__file__).parent / "data/tests/data/pixel_classifier_2D.ilp"
+    ).as_posix()
+
+    ilastik_pixel_classification_segmentation(
+        zarr_url=test_data_dir,
+        level=0,
+        channel=ChannelInputModel(label="DAPI"),
+        ilastik_model=str(ilastik_model),
+        output_label_name="test_label",
+    )
