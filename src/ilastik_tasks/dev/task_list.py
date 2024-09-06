571f959cc60d5238e78967466c8b9b14f992401f
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-06 14:59:44 +0200

    rename correctly task

--- src/ilastik_tasks/dev/task_list.py
+++ src/ilastik_tasks/dev/task_list.py
@@ -4,7 +4,7 @@ from fractal_tasks_core.dev.task_models import ParallelTask
 
 TASK_LIST = [
     ParallelTask(
-        name="Thresholding Label Task",
+        name="Ilastik Pixel Classification Segmentation",
         executable="ilastik_pixel_classification_segmentation.py",
         meta={"cpus_per_task": 8, "mem": 8000},
     ),
