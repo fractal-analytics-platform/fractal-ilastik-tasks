fcab58a68ff228fd72f6d8b643d2ed08a9dbb4f9
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-06 14:25:57 +0200

    fix project path

--- tests/test_ilastik_pixel_classification_segmentation.py
+++ tests/test_ilastik_pixel_classification_segmentation.py
@@ -22,27 +22,11 @@ def test_data_dir(tmp_path: Path) -> str:
     return dest_dir
 
 
-@pytest.fixture(scope="function")
-def test_ilastik_model(tmp_path: Path) -> str:
-    """
-    Copy a test-data folder into a temporary folder.
-    """
-    source_dir = (
-        Path(__file__).parent / "data/tests/data/pixel_classifier_2D.ilp"
-    ).as_posix()
-    dest_dir = (tmp_path / "my_image").as_posix()
-    debug(source_dir, dest_dir)
-    shutil.copytree(source_dir, dest_dir)
-    return dest_dir
-
-
-def test_thresholding_label_task(test_data_dir):
+def test_ilastik_pixel_classification_segmentation_task(test_data_dir):
     """
     Test the ilastik_pixel_classification_segmentation task.
     """
-    ilastik_model = (
-        Path(__file__).parent / "data/tests/data/pixel_classifier_2D.ilp"
-    ).as_posix()
+    ilastik_model = (Path(__file__).parent / "data/pixel_classifier_2D.ilp").as_posix()
 
     ilastik_pixel_classification_segmentation(
         zarr_url=test_data_dir,
