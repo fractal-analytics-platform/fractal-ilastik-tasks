97f1bfd70c385c96e775848a7cef2c225b0c5d30
jluethi <joel.luethi@uzh.ch>
2024-12-13 10:41:05 +0100

    Add docs_link and update CI

--- src/ilastik_tasks/dev/create_manifest.py
+++ src/ilastik_tasks/dev/create_manifest.py
@@ -7,4 +7,8 @@ from fractal_tasks_core.dev.create_manifest import create_manifest
 if __name__ == "__main__":
     PACKAGE = "ilastik_tasks"
     AUTHORS = "Lorenzo Cerrone"
-    create_manifest(package=PACKAGE, authors=AUTHORS)
+    docs_link = "https://github.com/fractal-analytics-platform/fractal-ilastik-tasks"
+    if docs_link:
+        create_manifest(package=PACKAGE, authors=AUTHORS, docs_link=docs_link)
+    else:
+        create_manifest(package=PACKAGE, authors=AUTHORS)
