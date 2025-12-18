dde232803d163838fcb7877877ddf4d419208bf1
lorenzo <lorenzo.cerrone@uzh.ch>
2025-12-18 10:00:42 +0100

    add new pydantic models to manifest building

--- src/ilastik_tasks/dev/task_list.py
+++ src/ilastik_tasks/dev/task_list.py
@@ -5,6 +5,30 @@ from fractal_task_tools.task_models import ParallelTask
 AUTHORS = "Lorenzo Cerrone"
 DOCS_LINK = "https://github.com/fractal-analytics-platform/fractal-ilastik-tasks"
 
+INPUT_MODELS = [
+    ("ngio", "images/_image.py", "ChannelSelectionModel"),
+    (
+        "ilastik_tasks",
+        "ilastik_utils.py",
+        "MaskingConfiguration",
+    ),
+    (
+        "ilastik_tasks",
+        "ilastik_utils.py",
+        "IteratorConfiguration",
+    ),
+    (
+        "ilastik_tasks",
+        "ilastik_utils.py",
+        "AdvancedIlastikParameters",
+    ),
+    (
+        "ilastik_tasks",
+        "ilastik_utils.py",
+        "IlastikChannels",
+    ),
+]
+
 TASK_LIST = [
     ParallelTask(
         name="Ilastik Pixel Classification Segmentation",
