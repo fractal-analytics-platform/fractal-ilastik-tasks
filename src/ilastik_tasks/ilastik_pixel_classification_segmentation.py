7fb88f25a1dac2bff223d523a39be7cc8b9ec38c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-09-02 15:44:47 +0200

    Alternative option: `logfile=/dev/null`

--- src/ilastik_tasks/ilastik_pixel_classification_segmentation.py
+++ src/ilastik_tasks/ilastik_pixel_classification_segmentation.py
@@ -62,9 +62,12 @@ def _setup_ilastik_logging():
     """
     import ilastik.ilastik_logging.default_config as default_config
 
-    Path(default_config.DEFAULT_LOGFILE_PATH).touch()
+    # Path(default_config.DEFAULT_LOGFILE_PATH).touch()
 
-    default_config.init(output_mode=default_config.OutputMode.CONSOLE)
+    default_config.init(
+        output_mode=default_config.OutputMode.CONSOLE,
+        logfile_path="/dev/null",
+    )
 
     _logger_name = "lazyflow.operators.classifierOperators.OpBaseClassifierPredict"
     _logger = logging.getLogger(_logger_name)
